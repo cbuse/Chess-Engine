55b4a3f4c999a5b8f8f6175ed85d1de666e82478
cbuse <cristina_buse@yahoo.com>
2014-07-21 23:28:27 +0300

    Create Modes.cpp

---
+++ Modes.cpp
@@ -0,0 +1,12 @@
+// Zugzwang Chess Engine - stage 2
+
+namespace Modes {
+	bool XBOARD_MODE = false;
+	bool FORCE_MODE = true;
+	// the playing color
+	bool WHITE_MODE = false;
+	bool BLACK_MODE = false;
+	// the color on the move
+	bool WHITE_MOVING = false;
+	bool BLACK_MOVING = false;
+};
