e849a7c89782271f8d5db548cb97f9cb5e221d93
cbuse <cristina_buse@yahoo.com>
2014-07-21 23:28:45 +0300

    Create Modes.h

---
+++ Modes.h
@@ -0,0 +1,15 @@
+// Zugzwang Chess Engine - stage 2
+
+#ifndef _MODES_H
+#define _MODES_H
+
+// Flags for different playing modes.
+namespace Modes {
+	extern bool XBOARD_MODE;
+	extern bool FORCE_MODE;
+	extern bool WHITE_MODE;
+	extern bool BLACK_MODE;
+	extern bool WHITE_MOVING;
+	extern bool BLACK_MOVING;
+};
+#endif
